db0478c70c16ef995134a543024c6226731847d2
Yulia <juliet-st0907@mail.ru>
2021-11-08 22:41:24 +0600

    Домашнее задание 4

--- src/main/java/org/example/lesson4/Triangle.java
+++ src/main/java/org/example/lesson4/Triangle.java
@@ -3,8 +3,16 @@ package org.example.lesson4;
 public class Triangle {
     public static double calculateTriangleSquare(double a, double b, double c) throws Exception {
         if (a <= 0 || b <= 0 || c <= 0) throw new Exception();
+        if ((a + b) <= c || (a + c) <= b || (c + b) <= a) throw new Exception();
+        if (c == 'a') throw new Exception();
         double halfPerimeter = (a + b + c) / 2;
         return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+
+
     }
+
+
+
+
 
 }
